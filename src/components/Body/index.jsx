d7905af641fd147dd80bd769278486c4e00b111f
tanluc1e <t.luc497303@gmail.com>
2023-12-15 21:05:55 +0700

    setup: Responsive for Body

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Top from './Top'
+import Listing from './Listing'
+import Activity from './Activity'
 import './style.css'
 
 export default function Body() {
     return (
-        <div>index</div>
+        <div className='mainContent'>
+            <Top />
+
+            <div className='bottom flex'>
+                <Listing />
+                <Activity />
+            </div>
+        </div>
     )
 }
