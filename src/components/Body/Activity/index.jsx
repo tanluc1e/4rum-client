4180d4bb4fa52f11fb778dc2433e9f0af8ce5ea6
tanluc1e <t.luc497303@gmail.com>
2023-12-15 12:08:44 +0700

    setup client

---
+++ src/components/Body/Activity/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export default function Activity() {
+    return (
+        <div>index</div>
+    )
+}
---
+++ src/components/Body/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export default function Body() {
+    return (
+        <div>index</div>
+    )
+}
